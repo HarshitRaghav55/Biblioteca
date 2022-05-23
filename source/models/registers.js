4c33909949df5c8a1951e97705696f5cf7e2cc51
harshit <harshit.raghav_cs19@gla.ac.in>
2022-05-23 21:58:59 +0530

    added file rgister making schema

---
+++ source/models/registers.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const readerSchema = new mongoose.Schema({
+    firstname:{
+        type:String,
+        required:true
+    },
+    lastname:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    phone:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    confirmPassword:{
+        type:String,
+        required:true
+    }
+})
+
+const Register = new mongoose.model("Reader",readerSchema);
+module.exports=Register;
