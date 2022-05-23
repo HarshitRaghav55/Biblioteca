1cc13dcf46bb93c911a9f0941f38f51bf6d09bf3
harshit <harshit.raghav_cs19@gla.ac.in>
2022-05-23 21:45:27 +0530

    register page and backend templates

--- source/app.js
+++ source/app.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const path= require("path");
 const app=express();
-
+const hbs = require("hbs")
 require("./database/connection")
 const port= process.env.PORT || 3000;
 // console.log(path.join(__dirname,"../public"));
 const staic_path=path.join(__dirname,"../public")
+const template_path=path.join(__dirname,"../templates/views")
+const partials_path=path.join(__dirname,"../templates/partials")
 app.use(express.static(staic_path))
+app.set("view engine", "hbs")
+app.set("views",template_path)
+hbs.registerPartials(partials_path)
+
 app.get('/',(req,res)=>{
-    res.send("hello login  ")
+    res.render("index")
 });
+// app.get('/login',(req,res)=>{
+//     res.render("login")
+// })
+app.get('/register',(req,res)=>{
+    res.render('register')
+})
 app.listen(port,()=>{
     console.log(`server is running at ${port}`);
 }); 
--- source/database/connection.js
+++ source/database/connection.js
@@ -2,5 +2,5 @@ const mongoose=require("mongoose");
 mongoose.connect("mongodb://localhost:27017/biblioteca").then(() =>{
     console.log("connection successfull");
 }).catch((e)=>{
-    console.log(e)
+    console.log("connection unscuccessful")
 })
