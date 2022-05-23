3706e29552e728ff513d1becfb28a67275ec3124
harshit <harshit.raghav_cs19@gla.ac.in>
2022-05-23 22:34:24 +0530

    data post for mongo

--- source/app.js
+++ source/app.js
@@ -3,11 +3,15 @@ const path= require("path");
 const app=express();
 const hbs = require("hbs")
 require("./database/connection")
+const Register = require("./models/registers");
+const async = require('hbs/lib/async');
 const port= process.env.PORT || 3000;
 // console.log(path.join(__dirname,"../public"));
 const staic_path=path.join(__dirname,"../public")
 const template_path=path.join(__dirname,"../templates/views")
 const partials_path=path.join(__dirname,"../templates/partials")
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
 app.use(express.static(staic_path))
 app.set("view engine", "hbs")
 app.set("views",template_path)
@@ -21,6 +25,21 @@ app.get('/',(req,res)=>{
 // })
 app.get('/register',(req,res)=>{
     res.render('register')
+})
+app.post('/register',async (req,res)=>{
+    try{
+        const registerReader = new Register({
+            firstname:req.body.firstname,
+            lastname:req.body.lastname,
+            email:req.body.email,
+            phone:req.body.phone,
+            password:req.body.password
+        })
+        const register = await(registerReader.save());
+        res.status(201).render("index")
+    } catch(error){
+        res.status(400).send(error)
+    }
 })
 app.listen(port,()=>{
     console.log(`server is running at ${port}`);
